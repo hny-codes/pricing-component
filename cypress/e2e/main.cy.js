6f9e37040b9dbb0179f0bcf232eeb677a39ac086
Henry <hnguyhenry@protonmail.com>
2024-01-08 08:51:21 -0800

    Add cypress test library

---
+++ cypress/e2e/main.cy.js
@@ -0,0 +1,10 @@
+describe('template spec', () => {
+  // beforeEach(() => {
+  //   cy.visit('/');
+  //   cy.viewport('iphone-8');
+  // });
+
+  it('Should passes', () => {
+    cy.visit('http://localhost:4321');
+  })
+});
