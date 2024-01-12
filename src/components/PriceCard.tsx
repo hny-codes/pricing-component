a8315d4c49eef4fcd33236dcce3fd77ca805bf71
Henry <hnguyhenry@protonmail.com>
2024-01-12 01:18:12 -0800

    Add PriceCard component with styling

--- src/components/PriceCard.tsx
+++ src/components/PriceCard.tsx
@@ -17,6 +17,7 @@ type Props = {
   sendAmount: number;
   sendType: 'GB' | 'TB';
   check: 'unchecked' | 'checked' | undefined;
+  style: 'empty' | 'filled';
 };
 
 export const PriceCard = ({
@@ -29,33 +30,62 @@ export const PriceCard = ({
   sendAmount,
   sendType,
   check,
+  style,
 }: Props) => {
   return (
-    <Card className='flex flex-col items-center text-center'>
+    <Card
+      className={`flex flex-col items-center text-center ${
+        style === 'filled'
+          ? 'bg-gradient-to-br from-[--clr-gradient-1] to-[--clr-gradient-2]'
+          : 'bg-white'
+      }`}
+    >
       <CardHeader>
-        <CardTitle>{title}</CardTitle>
+        <CardTitle
+          className={`${
+            style === 'filled'
+              ? 'text-[--clr-neutral-1]'
+              : 'text-[--clr-neutral-4]'
+          }`}
+        >
+          {title}
+        </CardTitle>
       </CardHeader>
-      <CardContent>
-        <div>
-          <div className='pb-6'>
-            <strong className='text-5xl'>
-              <span className='text-3xl'>$</span>
+      <CardContent className='w-full'>
+        <div
+          className={`${
+            style === 'filled'
+              ? 'text-[--clr-neutral-1]'
+              : 'text-[--clr-neutral-4]'
+          }`}
+        >
+          <div className='pb-8'>
+            <strong className='text-7xl relative'>
+              <span className='text-3xl absolute -left-5 top-1/3'>$</span>
               {check === 'checked' ? annualPrice : monthlyPrice}
             </strong>
           </div>
-          <div className='flex flex-col gap-4'>
+          <div className='flex flex-col gap-4 divide-y [&>*]:pt-3 border-t border-b font-bold'>
             <p>
               {storage} {storageType} Storage
             </p>
             <p>{users} Users Allowed</p>
-            <p>
+            <p className='pb-4'>
               Send up to {sendAmount} {sendType}
             </p>
           </div>
         </div>
       </CardContent>
-      <CardFooter>
-        <Button variant='outline'>Learn More</Button>
+      <CardFooter className='w-full pb-8'>
+        {style === 'empty' ? (
+          <Button variant='empty' size={'lg'} className='w-full'>
+            Learn More
+          </Button>
+        ) : (
+          <Button variant='filled' size={'lg'} className='w-full'>
+            Learn More
+          </Button>
+        )}
       </CardFooter>
     </Card>
   );
